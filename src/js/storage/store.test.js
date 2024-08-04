44a4e0d74c5993672d1ea7e378e4a5b94440a5cd
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-08-04 11:07:30 +0200

    unit testing save funciton

--- src/js/storage/store.test.js
+++ src/js/storage/store.test.js
@@ -1,26 +1,53 @@
 import { save } from './store.js';
 
 describe('save', () => {
-  let setItemSpy;
-
+  let key;
   beforeEach(() => {
     global.localStorage = {
       setItem: jest.fn(),
-      getItem: jest.fn(),
-      clear: jest.fn(),
     };
 
-    setItemSpy = jest.spyOn(global.localStorage, 'setItem');
-
+    key = 'testKey';
     jest.clearAllMocks();
   });
 
   it('should save an object to localStorage', () => {
-    const key = 'testKey';
     const value = { test: 'test' };
 
     save(key, value);
 
-    expect(setItemSpy).toHaveBeenCalledWith(key, JSON.stringify(value));
+    expect(localStorage.setItem).toHaveBeenCalledWith(key, JSON.stringify(value));
+  });
+
+  it('should save an array to localStorage', () => {
+    const value = [1, 2, 3, 4];
+
+    save(key, value);
+
+    expect(localStorage.setItem).toHaveBeenCalledWith(key, JSON.stringify(value));
+  });
+
+  it('should save a string to localStorage', () => {
+    const value = 'testString';
+
+    save(key, value);
+
+    expect(localStorage.setItem).toHaveBeenCalledWith(key, JSON.stringify(value));
+  });
+
+  it('should save a number to localStorage', () => {
+    const value = 123;
+
+    save(key, value);
+
+    expect(localStorage.setItem).toHaveBeenCalledWith(key, JSON.stringify(value));
+  });
+
+  it('should save a boolean to localStorage', () => {
+    const value = true;
+
+    save(key, value);
+
+    expect(localStorage.setItem).toHaveBeenCalledWith(key, JSON.stringify(value));
   });
 });
